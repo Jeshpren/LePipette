c61ad5a86bb524dc43c9854e296632edb396e1f5
Jeshpren <aljaz.zirk@gmail.com>
2023-08-26 08:16:25 +0200

    Added Project

---
+++ LePipette/LePipette/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LePipette;
+using System.Runtime.CompilerServices;
+
+Pipette _mainPipette = new Pipette(1, "Pepca");
+
+
+string _pipetteNickName = TestSecondsScript.FindNickname(_mainPipette.name);
+
+
+Print(_pipetteNickName);
+
+void Print(string printString)
+{
+    Console.WriteLine(printString);
+}
+class Pipette
+{
+    public int id;
+    public string name;
+
+    public Pipette(int id, string name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+}
