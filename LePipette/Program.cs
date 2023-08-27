73b49e7daedca13929a6a3a1d560dc4269836824
Jeshpren <aljaz.zirk@gmail.com>
2023-08-28 00:11:58 +0200

    first fit kinda works --> remove hardcodes

--- LePipette/Program.cs
+++ LePipette/Program.cs
@@ -1,7 +1,7 @@
 ﻿//using System.Runtime.CompilerServices;
 
 #region variables
-InputParams testInput = new InputParams(
+InputParams testInput1 = new InputParams(
     96,
     new string[][] { new[] { "Sam1", "Sam2", "Sam3" }, new[] { "Sam1", "Sam3", "Sam4" } },
     new string[][] { new[] { "ReagX", "ReagY" }, new[] { "ReagY", "ReagZ" } },
@@ -15,11 +15,18 @@ InputParams testInput2 = new InputParams(
     new int[] { 3, 1, 2, 3, 1 },
     1
     );
+InputParams testInput3 = new InputParams(
+    96,
+    new string[][] { new[] { "Sam1", "Sam2", "Sam3" }, new[] { "Sam7" }, new[] { "Sam4", "Sam5", "Sam6" }, new[] { "Sam8", "Sam9" }, new[] { "Sa10", "Sa11", "Sa12" }, new[] { "Sa13", "Sa14", "Sa15", "Sa16" }, new[] { "Sa17", "Sa18", "Sa19" } },
+    new string[][] { new[] { "ReagA" }, new[] { "ReagD", "ReagE", "ReagF" }, new[] { "ReagB", "ReagC" }, new[] { "ReagG", "ReagH" }, new[] { "ReagI" }, new[] { "ReagJ", "ReagK" }, new[] { "ReagL" } },
+    new int[] { 3, 1, 2, 3, 1, 2, 5 },
+    1
+    );
 #endregion
 
 #region MAIN PROGRAM EXECUTION
 
-Well[,] result = GenerateOptimalWellPlacement(testInput2);
+Well[,] result = GenerateOptimalWellPlacement(testInput3);
 
 #endregion
 
@@ -27,20 +34,116 @@ Well[,] result = GenerateOptimalWellPlacement(testInput2);
 #region methods
 Well[,] GenerateOptimalWellPlacement(InputParams inputParams)
 {
-    Well[,] outputParams = CreateEmtpyPlate(inputParams.plateSize);
+    int plateRows = 0;
+    int plateCols = 0;
+    if (inputParams.plateSize == 96)
+    {
+        plateRows = 8;
+        plateCols = 12;
+    }
+    else if (inputParams.plateSize == 384)
+    {
+        plateRows = 16;
+        plateCols = 24;
+    }
 
-    // todo 1. generate array of 2d arrays (these will be the starting groups of wells (group of wells = experiment) that will then get sorted)
+    //x todo 1. generate array of 2d arrays (these will be the starting groups of wells (group of wells = experiment) that will then get sorted)
     Experiment[] experiments = GenerateExperimentArray(inputParams);
+    // todo ce je experiment vecji od plate-a ga razrez
     PrintExperimentArray(experiments);
 
-    // todo 2. find an algorithm for optimal space usage
     // sort experiment array: primarily sort by height, if heights match then sort by width 
-    Array.Sort(experiments, (a, b) => a.wells.GetLength(1) == b.wells.GetLength(1) ? b.wells.GetLength(0).CompareTo(a.wells.GetLength(0)) : b.wells.GetLength(1).CompareTo(a.wells.GetLength(1)));
-
+    Array.Sort(experiments, (a, b) => a.wells.GetLength(0) == b.wells.GetLength(0) ? b.wells.GetLength(1).CompareTo(a.wells.GetLength(1)) : b.wells.GetLength(0).CompareTo(a.wells.GetLength(0)));
     Console.WriteLine("SORTED:\n");
     PrintExperimentArray(experiments);
 
-    return outputParams;
+    // todo 2. find an algorithm for optimal space usage
+    // torej: First Fit... ce je na trenutni poziciji dost placa, insertej experiment, drgac pejt v nasledno vrstico
+    Well[,] plate = CreateEmtpyPlate(inputParams.plateSize);
+    Well[,] plate1 = CreateEmtpyPlate(inputParams.plateSize);
+    int currentRow = 0;
+    int currentCol = 0;
+    for (int i = 0; i < experiments.Length; i++)
+    {
+        int experimentRows = experiments[i].wells.GetLength(0);
+        int experimentCols = experiments[i].wells.GetLength(1);
+        while (!(experimentCols < plateCols - currentCol && experimentRows < plateRows - currentRow))
+        {
+            currentCol += experimentCols;
+            if (currentCol >= plateCols)
+            {
+                currentCol = 0;
+                while (plate[currentRow, 0] != null)
+                {
+                    currentRow++;
+                    if (experimentRows >= plateRows - currentRow)
+                    {
+                        currentRow = 0;
+                        currentCol = 0;
+                        // * zdej dela --> nared array platov (glede na max plates)
+                        plate1 = plate;
+                        plate = CreateEmtpyPlate(inputParams.plateSize);
+                        break;
+                    }
+                }
+            }
+        }
+        // put experiment on plate
+        for (int rowIdx = 0; rowIdx < experimentRows; rowIdx++)
+        {
+            for (int columnIdx = 0; columnIdx < experimentCols; columnIdx++)
+            {
+                plate[currentRow + rowIdx, currentCol + columnIdx] = experiments[i].wells[rowIdx, columnIdx];
+            }
+        }
+        currentCol += experimentCols;
+        if (currentCol >= plateCols)
+        {
+            currentCol = 0;
+            while (plate[currentRow, 0] != null)
+            {
+                currentRow++;
+            }
+        }
+    }
+
+    // PRINT PLATE
+    Console.WriteLine("PLATE:");
+    string[] stringsToPrint = new string[plateRows];
+    for (int i = 0; i < plateRows; i++)
+    {
+        for (int j = 0; j < plateCols; j++)
+        {
+            if (plate[i, j] != null)
+            {
+                stringsToPrint[i] += "[" + plate[i, j].sample + "-" + plate[i, j].reagent + "] ";
+            }
+            else
+            {
+                stringsToPrint[i] += "             ";
+            }
+        }
+        Console.WriteLine(stringsToPrint[i]);
+    }
+    Console.WriteLine("PLATE1:");
+    stringsToPrint = new string[plateRows];
+    for (int i = 0; i < plateRows; i++)
+    {
+        for (int j = 0; j < plateCols; j++)
+        {
+            if (plate1[i, j] != null)
+            {
+                stringsToPrint[i] += "[" + plate1[i, j].sample + "-" + plate1[i, j].reagent + "] ";
+            }
+            else
+            {
+                stringsToPrint[i] += "             ";
+            }
+        }
+        Console.WriteLine(stringsToPrint[i]);
+    }
+
+    return plate;
 }
 
 Experiment[] GenerateExperimentArray(InputParams inputParams)
@@ -48,11 +151,11 @@ Experiment[] GenerateExperimentArray(InputParams inputParams)
     //create the experiment array
     int nOfExperiments = inputParams.nOfReplicates.Length;
     Experiment[] experimentArray = new Experiment[nOfExperiments];
-    for (int i = 0; i < inputParams.reagentNames.Length; i++)
+    for (int i = 0; i < nOfExperiments; i++)
     {
-        int experimentHeight = inputParams.sampleNames[i].Length;
-        int experimentWidth = inputParams.reagentNames[i].Length * inputParams.nOfReplicates[i];
-        experimentArray[i] = new Experiment(new Well[experimentWidth, experimentHeight]);
+        int experimentRows = inputParams.sampleNames[i].Length;
+        int experimentCols = inputParams.reagentNames[i].Length * inputParams.nOfReplicates[i];
+        experimentArray[i] = new Experiment(new Well[experimentRows, experimentCols]);
         for (int j = 0; j < inputParams.reagentNames[i].Length; j++)
         {
             for (int k = 0; k < inputParams.sampleNames[i].Length; k++)
@@ -60,7 +163,7 @@ Experiment[] GenerateExperimentArray(InputParams inputParams)
                 int replicate = 0;
                 for (int l = 0; l < inputParams.nOfReplicates[i]; l++)
                 {
-                    experimentArray[i].wells[replicate + j * inputParams.nOfReplicates[i], k] = new Well(inputParams.sampleNames[i][k], inputParams.reagentNames[i][j]);
+                    experimentArray[i].wells[k, replicate + j * inputParams.nOfReplicates[i]] = new Well(inputParams.sampleNames[i][k], inputParams.reagentNames[i][j]);
                     replicate++;
                 }
             }
@@ -72,15 +175,15 @@ void PrintExperimentArray(Experiment[] experimentArray)
 {
     for (int i = 0; i < experimentArray.Length; i++)
     {
-        int experimentHeight = experimentArray[i].wells.GetLength(1);
-        int experimentWidth = experimentArray[i].wells.GetLength(0);
-        string[] stringsToPrint = new string[experimentArray[i].wells.GetLength(1)];
-        for (int j = 0; j < experimentHeight; j++)
+        int experimentRows = experimentArray[i].wells.GetLength(0);
+        int experimentCols = experimentArray[i].wells.GetLength(1);
+        string[] stringsToPrint = new string[experimentArray[i].wells.GetLength(0)];
+        for (int j = 0; j < experimentRows; j++)
         {
-            for (int k = 0; k < experimentWidth; k++)
+            for (int k = 0; k < experimentCols; k++)
             {
-                string sample = experimentArray[i].wells[k, j].sample;
-                string reagent = experimentArray[i].wells[k, j].reagent;
+                string sample = experimentArray[i].wells[j, k].sample;
+                string reagent = experimentArray[i].wells[j, k].reagent;
                 stringsToPrint[j] += "[" + sample + "-" + reagent + "] ";
             }
             Console.WriteLine(stringsToPrint[j]);
@@ -88,58 +191,6 @@ void PrintExperimentArray(Experiment[] experimentArray)
         Console.WriteLine("");
     }
 }
-//Well[,] GenerateExperimentArrayOLD(InputParams inputParams)
-//{
-//    // calculate dimensions of experiment array
-//    int height = 0;
-//    int width = 0;
-//    for (int i = 0; i < inputParams.sampleNames.Length; i++)
-//        if (inputParams.sampleNames[i].Length > height)
-//            height = inputParams.sampleNames[i].Length;
-//    for (int i = 0; i < inputParams.reagentNames.Length; i++)
-//        width += inputParams.reagentNames[i].Length * inputParams.nOfReplicates[i];
-
-//    Console.WriteLine("Experiment Arary size: " + width + "x" + height);
-//    // create and fill the experiment array
-//    Well[,] experimentArray = new Well[width, height];
-//    int currentWidth = 0;
-//    for (int i = 0; i < inputParams.reagentNames.Length; i++)
-//    {
-//        for (int j = 0; j < inputParams.reagentNames[i].Length; j++)
-//        {
-//            for (int k = 0; k < inputParams.sampleNames[i].Length; k++)
-//            {
-//                int replicate = 0;
-//                for (int l = 0; l < inputParams.nOfReplicates[i]; l++)
-//                {
-//                    experimentArray[currentWidth + replicate, k] = new Well(inputParams.sampleNames[i][k], inputParams.reagentNames[i][j]);
-//                    replicate++;
-//                }
-//            }
-//            currentWidth += inputParams.nOfReplicates[i];
-//        }
-//    }
-//    // print experiment array in console
-//    string[] stringsToPrint = new string[height];
-//    for (int i = 0; i < experimentArray.GetLength(1); i++)
-//    {
-//        for (int j = 0; j < experimentArray.GetLength(0); j++)
-//        {
-//            if (experimentArray[j, i] != null)
-//            {
-//                stringsToPrint[i] += "[" + experimentArray[j, i].sample + "-" + experimentArray[j, i].reagent + "] ";
-//            }
-//            else
-//            {
-//                stringsToPrint[i] += "             ";
-//            }
-//        }
-//        Console.WriteLine(stringsToPrint[i]);
-//    }
-
-//    return experimentArray;
-//}
-
 Well[,] CreateEmtpyPlate(int plateSize)
 {
     Well[,] outputPlate;
